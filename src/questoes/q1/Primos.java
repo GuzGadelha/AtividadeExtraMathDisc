aacf9eb0987abe8b962eca2802913484c2e45b72
GuzGadelha <Guztv04@gmail.com>
2025-07-08 15:32:24 -0300

    update: primos foi melhorado

--- src/questoes/q1/Primos.java
+++ src/questoes/q1/Primos.java
@@ -3,30 +3,46 @@ package questoes.q1;
 import java.util.Scanner;
 
 public class Primos {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);    //  Adicionando scanner para ler entrada do usuário
-        System.out.println("Digite um número positivo acima de \"2\": ");   //  Perguntando e lendo entrada do usuário
-        int num = in.nextInt();
-        num = Math.abs(num);                 //  Caso o usuário digite um número negativo eu recebo o valor absoluto, ou seja, entendo como sendo positivo
-        boolean primo = true;                //  Declarando bool para ser ou não primo,
-        int cont = 0;                        //  contador inteiro para contar a quantidade de divisores,
-        int totalPrimos = 0;                 // Contador dqa quantidade total de primos
-        for (int i = 2; i <= num; i++){      //  Laço de repetição FOR para percorrer de 2 ao limite(num)
-            for (int j = 1; j <= i; j++){    //  Laço para testar se o "i" é primo ou não
-                if (i % j == 0){             //  Condicional para saber se é primo
-                    cont++;
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Digite um número inteiro positivo (≥ 2): ");
+            if (!in.hasNextInt()) {
+                System.out.println("Entrada inválida. Digite um número inteiro.");
+                return;
+            }
+
+            int num = Math.abs(in.nextInt());
+            if (num < 2) {
+                System.out.println("Por favor, digite um número maior ou igual a 2.");
+                return;
+            }
+
+            int totalPrimos = 0;
+
+            for (int i = 2; i <= num; i++) {
+                boolean primo = isPrimo(i);
+                System.out.println("P(" + i + ") = " + primo);
+                if (primo) {
+                    totalPrimos++;
                 }
             }
-            if (cont > 2){                   // Se for divisivel por mais de 2, false. Caso contrario, true
-                primo = false;
-            } else{
-                primo = true;
-                totalPrimos++;
-            }
-            cont = 0;
-            System.out.println("P(" + i + ") = " + primo);
-        }
-        System.out.println("Quantidade total de primos entre 2 e " + num + " = " + totalPrimos);
 
+            System.out.println("Quantidade total de primos entre 2 e " + num + " = " + totalPrimos);
+        }
+    }
+
+    public static boolean isPrimo(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        int raiz = (int) Math.sqrt(n);
+        for (int i = 3; i <= raiz; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
